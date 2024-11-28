948df6a8a7e26d39ef5f22802c5d7b4633a1ee01
Ayush Sharma <141551003+ayushh201@users.noreply.github.com>
2024-11-28 21:18:47 +0530

    Create 2DmatrixDijkstra.cpp

---
+++ CSES_GRAPH/2DmatrixDijkstra.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    #define P pair<int,pair<int,int>>
+    vector<vector<int>> directions={{0,1},{0,-1},{1,0},{-1,0}};
+    
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> result(m,vector<int>(n,1e9));
+        result[0][0]=0;
+        priority_queue<P,vector<P>,greater<P>> pq;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            auto current=pq.top();
+            pq.pop();
+            
+            int d=current.first;
+            int i=current.second.first;
+            int j=current.second.second;
+            
+            for(auto &dir:directions){
+                int x=i+dir[0];
+                int y=j+dir[1];
+                if(x<0 || x>=m || y<0 || y>=n){
+                    continue;
+                }
+                int wt=grid[x][y]==1? 1:0;
+                if(d+wt<result[x][y]){
+                    result[x][y]=d+wt;
+                    pq.push({d+wt,{x,y}});
+                }
+            }
+        }
+        return result[m-1][n-1];
+    }
+};
